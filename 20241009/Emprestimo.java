eb4ac9c9e22b3d211442f5db43d16672e236308a
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-09 14:52:56 -0300

    Create Emprestimo.java

---
+++ 20241009/Emprestimo.java
@@ -0,0 +1,18 @@
+public class Emprestimo {
+    private double valorEmprestimo;
+    private int numeroParcelas;
+
+    public Emprestimo(double valorEmprestimo, int numeroParcelas) {
+        this.valorEmprestimo = valorEmprestimo;
+        this.numeroParcelas = numeroParcelas;
+    }
+
+    public double calcularValorParcela() {
+        return valorEmprestimo / numeroParcelas;
+    }
+
+    public boolean isAprovado(double salario) {
+        double valorParcela = calcularValorParcela();
+        return valorParcela <= (0.3 * salario);
+    }
+}
