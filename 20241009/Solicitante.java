bc919b32f3fdc03a33a4a2196da4dfebfbd30ffb
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-09 15:00:36 -0300

    Update Solicitante.java

--- 20241009/Solicitante.java
+++ 20241009/Solicitante.java
@@ -1,11 +1,13 @@
+package com.ifsc.ctds;
+
 public class Solicitante {
-    private double salario;
-
-    public Solicitante(double salario) {
-        this.salario = salario;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
+	private double salario;
+	
+	public Solicitante(double salario) {
+	this.salario = salario;
+	}
+	
+	public double getSalario() {
+		return salario;
+	}
 }
