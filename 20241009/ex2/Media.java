b22422d030ea4eff73f74e9c237a60a0030cd2b2
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-09 16:32:37 -0300

    Update Media.java

--- 20241009/ex2/Media.java
+++ 20241009/ex2/Media.java
@@ -1,1 +1,37 @@
+/*
+2 – Faça um programa onde serão informados as quatro notas do aluno. O programa irá então apresentar a
+média, se foi aprovado (nota ≥ 7) ou se ficou em exame.
+
+Caso o aluno ficou em exame, o programa irá então
+perguntar qual foi a nota do exame e então calculará a nova média (média anterior com a nota do exame) e
+informar se ele foi aprovado (nova média ≥ 5) ou se foi reprovado. 
+*/
+
+package com.ifsc.ctds;
+
+public class Media {
+	private double media;
+
+	public void calcularMedia(double somaNotas, int quantNotas) {
+		this.media = somaNotas / quantNotas;
+	}
+
+	public void imprimirMedia() {
+		System.out.println("A média : " + String.format("%.2f", this.media));
+	}
+	
+	public double getMedia() {
+		return this.media;
+	}
+	
+	public boolean  aprovado() {
+		return this.media >= 7;
+
+	}
+	public boolean  exame() {
+    return this.media >= 5 && this.media < 7;
+
+	}
+
+}
 
