593e53141871efddf9d6f8779a837f3f6615d06c
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-10 16:01:24 -0300

    Update entrada.java

--- 20241009/ex9/entrada.java
+++ 20241009/ex9/entrada.java
@@ -1,2 +1,42 @@
 /*9 – Fazer um algoritmo que calcule e imprima o soma, a média, o maior e o menor dos valores armazenados
 em um vetor A de 7 elementos numéricos a serem lidos do dispositivo de entrada padrão.*/
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        double[] A = new double[7]; // Vetor para armazenar os 7 elementos
+        double soma = 0;
+        double maior = Double.MIN_VALUE; // Inicializa com o menor valor possível
+        double menor = Double.MAX_VALUE;  // Inicializa com o maior valor possível
+        
+        // Lendo os valores do vetor
+        for (int i = 0; i < A.length; i++) {
+            System.out.print("Digite o valor " + (i + 1) + ": ");
+            A[i] = scanner.nextDouble();
+            soma += A[i]; // Acumulando a soma
+            
+            // Verificando o maior e menor valor
+            if (A[i] > maior) {
+                maior = A[i];
+            }
+            if (A[i] < menor) {
+                menor = A[i];
+            }
+        }
+        
+        // Calculando a média
+        double media = soma / A.length;
+        
+        // Imprimindo os resultados
+        System.out.println("Soma: " + soma);
+        System.out.println("Média: " + media);
+        System.out.println("Maior valor: " + maior);
+        System.out.println("Menor valor: " + menor);
+        
+        scanner.close();
+    }
+}
