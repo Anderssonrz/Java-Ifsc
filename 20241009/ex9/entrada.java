c9abc378184af8618e12b30a1fa6298f0dab04e2
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-10 14:33:20 -0300

    Create entrada.java

---
+++ 20241009/ex9/entrada.java
@@ -0,0 +1,2 @@
+/*9 – Fazer um algoritmo que calcule e imprima o soma, a média, o maior e o menor dos valores armazenados
+em um vetor A de 7 elementos numéricos a serem lidos do dispositivo de entrada padrão.*/
