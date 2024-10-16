c1ee13491fa6a3facf8285e1e15363e92fb996e5
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-16 16:43:22 -0300

    Add files via upload

--- 20241016/abstract/Animal.java
+++ 20241016/abstract/Animal.java
@@ -1,1 +1,17 @@
+package com.ifsc.ctds;
 
+public abstract class Animal {
+	private String nome;
+	
+	public abstract void emitirSom();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+}
