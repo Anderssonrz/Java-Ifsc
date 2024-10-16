ba5a7b2ba5aa7443b93b23f94d6a9cb849edcb1a
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-16 16:37:56 -0300

    Create TesteAnimal.java

---
+++ 20241016/abstrat/TesteAnimal.java
@@ -0,0 +1,13 @@
+package com.ifsc.ctds;
+
+public class TesteAnimal {
+
+	public static void main(String[] args) {
+		Cachorro rex = new Cachorro();
+		Gato garfeld = new Gato();
+		
+		rex.latir();
+		garfeld.miar();
+	}
+
+}
