9c96ec85f7d41adf006173c957f48e38b99ffbba
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-21 15:14:59 -0300

    Create TesteData.java

---
+++ 20241021/SimpleDateFormat/TesteData.java
@@ -0,0 +1,24 @@
+package com.ifsc.com;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+public class TesteData {
+
+	public static void main(String[] args) {
+		
+		Date hoje = new Date();
+		DateFormat df1 = null;
+		String formato = "dd/MM/yyyy";
+	
+		df1 = new SimpleDateFormat(formato);
+		
+		JOptionPane.showMessageDialog(null, df1.format(hoje));
+
+	
+	}
+
+}
