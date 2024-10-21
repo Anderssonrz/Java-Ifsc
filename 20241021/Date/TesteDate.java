bbc79f907fc56cbb572be34428496cb5d05ec697
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-10-21 14:44:35 -0300

    Create TesteDate.java

---
+++ 20241021/Date/TesteDate.java
@@ -0,0 +1,17 @@
+package com.ifsc.ctds;
+
+import java.util.Date;
+
+import javax.swing.JOptionPane;
+
+public class TesteDate {
+
+	
+	public static void main(String[] args) {
+		Date hoje = new Date();
+		
+		JOptionPane.showMessageDialog(null, hoje);
+
+	}
+
+}
